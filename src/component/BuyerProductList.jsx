b832276b893e018a2f225dbd080c6e95a82d3037
Arun1only1 <arun1nly1@gmail.com>
2024-04-22 14:07:22 +0545

    product card added

--- src/component/BuyerProductList.jsx
+++ src/component/BuyerProductList.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import ProductCard from "./ProductCard";
+import { Box } from "@mui/material";
 
 const BuyerProductList = () => {
-  return <div>BuyerProductList</div>;
+  return (
+    <Box>
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+    </Box>
+  );
 };
 
 export default BuyerProductList;
